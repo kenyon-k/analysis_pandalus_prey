e7eba658e3a782f06ee0c6841dd8fe1855fbd6fb
Krista <krista.kenyon@hotmail.com>
2024-07-10 12:00:24 -0500

    Initial Table

--- scripts/Script_Table01_Sample-Locations.R
+++ scripts/Script_Table01_Sample-Locations.R
@@ -56,14 +56,14 @@ library(tidyverse)
 library(dplyr)
 library(reshape2) 
 library(viridis)                 # Fig 5 - color blind friendly palette
-# library(ggrepel)               # formats figure labels so they don't overlap                   
+# library(ggrepel)               # formats figure labels so they don't overlap
+library(gt)                      # Table formatting
 
 
 
 ### import data
 
 prey <- read.csv('data/processed/2019_basePrey.csv')
-pred <- read.csv('data/processed/2019_basePred.csv')
 
 
 ################################################################################
@@ -97,15 +97,69 @@ pred <- read.csv('data/processed/2019_basePred.csv')
 
 ### create Table 1 dataframe:
 
+### checking how many unique sample locations occurred
+
+length(unique(prey$Trawl_ID))
+    # 2019 data: 92 observations
+
+
+
+### duplicating base prey dataframe
+
+samp <- prey
+
+
+
+### Retain one row per sampled Trawl ID
+
+samp <- samp[!duplicated(samp$Trawl_ID),]
+    # removing duplicated values for Trawl_ID. Results in one entry per trawl (i.e. sample locations)
+
+
+# CHECK NUMBER VARIABLES! Does it match # unique Trawl_ID sampled above
+
+
+str(samp) 
+    # for 2019 data: 92 observations
+
 
 
 ### Saving Final Dataframe Structure 
 
+# str(samp)
+
+# 'data.frame':	92 obs. of  24 variables:
+
+# $ X                 : int  1 26 29 35 48 59 121 179 184 196 ...
+# $ ContentKey        : chr  "62114003_892_3_1_-14_1733" "62114004_95_3_1_-36_1553" "62114013_892_3_1_-41_1691" "62114014_892_3_1_-63_380" ...
+# $ Prey_OS_ID        : int  6967 6000 8020 8112 451 8020 6000 9998 6996 9998 ...
+# $ PreyWt            : num  0.3 4.4 3.56 1.54 13.93 ...
+# $ Prey_Count        : int  NA NA 1 1 1 NA NA NA 1 NA ...
+# $ OS_ID             : int  892 95 892 892 892 89 794 794 89 792 ...
+# $ Length            : num  24.5 45.5 35 19 36.2 28 13.5 27.7 48 28 ...
+# $ Trawl_ID          : int  62114003 62114004 62114013 62114014 62114087 62114089 62114090 62114093 62114096 62114021 ...
+# $ Year              : int  2019 2019 2019 2019 2019 2019 2019 2019 2019 2019 ...
+# $ Start.Depth       : int  269 290 182 161 225 603 421 506 248 720 ...
+# $ End.Depth         : int  271 289 184 163 225 604 427 504 239 695 ...
+# $ Depth.Range       : chr  "201-300" "201-300" "0-200" "0-200" ...
+# $ Study.Area        : chr  "2G" "2G" "2G" "2G" ...
+# $ Scientific.Name   : chr  "REINHARDTIUS HIPPOGLOSSOIDES" "RAJA HYPERBOREA = AMBLYRAJA HYPERBOREA" "REINHARDTIUS HIPPOGLOSSOIDES" "REINHARDTIUS HIPPOGLOSSOIDES" ...
+# $ CommonName        : chr  "GREENLAND HALIBUT, TURBOT" "ARCTIC SKATE" "GREENLAND HALIBUT, TURBOT" "GREENLAND HALIBUT, TURBOT" ...
+# $ Start.Lat.Degree  : int  57 57 58 58 60 60 60 61 62 58 ...
+# $ Start.Lat.Minutes : num  45.1 48.4 33.4 32.5 32.3 ...
+# $ Start.Long.Degree : int  60 60 62 61 65 65 65 63 63 59 ...
+# $ Start.Long.Minutes: num  24.16 23.56 5.16 56.02 57.33 ...
+# $ FishKey           : chr  "62114003_892_3_1_-14" "62114004_95_3_1_-36" "62114013_892_3_1_-41" "62114014_892_3_1_-63" ...
+# $ region            : chr  "SFA4" "SFA4" "SFA4" "SFA4" ...
+# $ pred.name         : chr  "Greenland halibut" "Skate" "Greenland halibut" "Greenland halibut" ...
+# $ depth             : chr  "200-300 m" "200-300 m" "100-200 m" "100-200 m" ...
+# $ length.range      : chr  "21-25" "41-45" "31-35" "16-20" ...
 
 
-### Exporting Table 1 Dataframe
 
+### Exporting Table 1 Sample Location Dataframe
 
+write.csv(samp, file="data/processed/2019_T1_sampleLocation.csv")
 
 ###################    Building and Formatting Table 1    ######################
 
@@ -116,9 +170,49 @@ pred <- read.csv('data/processed/2019_basePred.csv')
 ### Load Table 1 dataframe
 
 
+#samp <- read.csv('data/processed/2019_T1_sampleLocation.csv')
+
+
+# Below doesn't group it the way I want for the table. Which is  what the default table() provides
+samp.loc <- samp %>%
+  select(depth, region)
+
+view(samp.loc)
+
+
+### create Table 1 dataframe: sum survey locations per regions, per depth
+
+# formula will need something to sum. trawl.sample is the number of trawls per trawl_id (or location)
+samp$trawl.sample <- 1 
+
+# reformatting the data into the structure gt() will want
+s1 <- dcast(samp, depth~region, value.var="trawl.sample", sum) %>%
+  relocate(SFA4, .after = WAZ)
+      # moves the SFA4 column after WAZ. the '.' in .after is important!
+      # used the dyplr cheat sheet and copilot
+
+
+view(s1)
+
+# checking that we still have correct # of survey locations
+sum(s1$EAZ + s1$SFA + s1$WAZ)
+  # 2019 data = 92 survey locations
+
+
+
 ### Table 1
 
+# table(samp$depth, samp$region) # option 1 - I think formatting will be tricky
 
+# I WILL NEED TO RE-ORDER EAZ, WAZ, and SFA4
+# I WILL ALSO WANT TO REMOVE THE 'm' FROM THE DEPTH COLUMN ROWS
+
+s1 %>%
+  gt() %>%
+  cols_label(depth ~ "Depth Stratum (m)",
+             SFA4 ~ "SFA 4")  %>% 
+  tab_spanner(label = "Assessment Area",
+              columns = EAZ:SFA4) 
 
 
 
--- scripts/draft code/Draft_Script_Table01_sample-location.R
+++ scripts/draft code/Draft_Script_Table01_sample-location.R
@@ -32,26 +32,222 @@
 
 
 ################################################################################
-#                             Table Code
+#                         Table Formatting - gt & gtExtra
 ################################################################################
 
 
-### Coding Category
+### Making Tables Pretty
 
-# (highlight details) website
-# Tutorial ?
-# What did I learn or use from it?
+    # (overview of gt) https://www.youtube.com/watch?v=jptX745mp7M
+        # Tutorial 1
+        # Really good overview about table manipulations and formatting in gt package
 
-
-###########################    Tutorial ?   ###################################
-
-
-# From ............... channel website
-
-# brief description
+    # (overview of gtExtra) https://www.youtube.com/watch?v=AJ0_PCIVXD4
+        # Tutorial 2
+        # Insert distribution graphs within the table
+        # a bit on themes
 
 
+###########################    Tutorial 1   ###################################
 
+
+# From Equitable Equations channel https://www.youtube.com/watch?v=jptX745mp7M
+
+# great overview of gt package
+
+
+library(gt)
+library(tidyverse)
+#install.packages('modeldata')
+library(modeldata)
+
+data(scat)
+
+# Data preprocessing
+
+scat_table1 <- scat %>% 
+  select(Species, 
+         Site, 
+         Length,
+         Diameter,
+         Mass,
+         d13C,
+         d15N) %>% 
+  group_by(Species) %>% 
+  summarize(across(where(is.numeric), 
+                   \(x) round(mean(x, na.rm = TRUE), 1)),
+            count = n())
+
+scat_table2 <- scat %>% 
+  select(Species, 
+         Location,
+         Site, 
+         Length,
+         Diameter,
+         Mass,
+         d13C,
+         d15N) %>% 
+  group_by(Species, Site) %>% 
+  summarize(across(where(is.numeric), 
+                   \(x) round(mean(x, na.rm = TRUE), 1)),
+            Count = n())
+
+
+## A great table
+
+# basic
+# his had shading in 1 & 3 row - mine doesn't. 
+# basic horizontal lines
+st <- scat_table1 %>%
+  gt()
+
+
+# Rename Columns
+st <- st %>%
+  cols_label(count ~ "Count",
+             Length ~ "length")
+
+# when adding something to the table, the notation is 'tab_xxxx'
+
+
+# Adding a Title (known as Table Header)
+st <- st %>%
+  tab_header(title = "California poopies",
+             subtitle = "Morphometric data on scat") 
+# two elements to a header: the title and subtitle
+
+
+# Add some source data at bottom of table
+# gt allows you to use markdown notation when writing text (headings, column titles, footnotes, etc)
+# this is the md() within the tab_source_note()
+# can also use html() to use html text formating
+st <- st %>%
+  tab_source_note(md("**Source**: Reid, R. E. B. (2015). A morphometric modeling approach to distinguishing among bobcat, coyote and gray fox scats. *Wildlife Biology*, 21(5), 254-262"))
+st
+
+
+# Add a spanner to the table
+# spanners = horizontal lines that only go through part of the table
+# need to specify 2 things: columns it will cover, and what the labels will be
+st <- st %>%
+  tab_spanner(label = "Physical averages",
+              columns = Length:Mass) 
+st
+
+# I can use tidyr select syntext when trying to specify which rows/columns something applies to
+# ex: above with tab_spanner(columns = Length:Mass)
+# ex: specify columns using numbers, names, using : for a span, helper verbs (includes, starts with)
+
+
+# offsetting the first column title (as if it's a row name)
+# normal pipe with 'st' won't work. I'm guessing it's because it's changing gt() itself
+# removes the name of the column, and adds a vertical line between the 1 and 2 column
+scat_table1 %>%
+  gt(rowname_col = "Species")  # formats Species column as row names
+
+
+# to add the first column title back in while keeping the 'row title' appearance
+scat_table1 %>%
+  gt(rowname_col = "Species") %>%
+  tab_stubhead(label = "Species")    # adds the row name back and left aligns it
+
+
+# tab_footnote -> adds a footnote. Look at helpfile for example.
+# need to say what the footnote will be, and where it will go (location)
+
+
+# fmt_xxxx = format commands for various metrics
+# this lets you manually format things
+# eg: format_percent = # should be %
+
+
+
+## A stylized version
+
+st1 <- scat_table1 %>% 
+  gt() %>% 
+  cols_label(count ~ "Count") %>% 
+  tab_header(title = "California poopies",
+             subtitle = "Morphometric data on scat") %>% 
+  tab_source_note(md("**Source**: Reid, R. E. B. (2015). A morphometric modeling approach to distinguishing among bobcat, coyote and gray fox scats. *Wildlife Biology*, 21(5), 254-262")) %>% 
+  tab_spanner(label = "Physical averages",
+              columns = Length:Mass) 
+
+# changes style guides
+# style 1 is the default
+# limited in the color palette - only has 7 colours
+st <- st %>%
+  opt_stylize(style = 2, color = "red")
+st
+
+
+# I can have more fine-grained controle than pre-created styles.
+# opt_xxx packages
+
+
+
+# Grouped data
+scat_table2 # has 3 groups within species
+
+# basic table with grouped data
+# values in grouped variables each have a row of their own
+# other variables below that
+scat_table2 %>%
+  gt()
+
+# If we ungroup first
+# it will strip out the group parameter, and there will be unique row for each species observations
+scat_table2 %>% 
+  ungroup() %>%
+  gt()
+
+# we can also let gt() know there are groups on the fly
+# we don't have to put a groupby() command if we want a grouped table
+scat_table2 %>% 
+  ungroup() %>%
+  gt(groupname_col = "Species") # identifies which column is grouped
+# gt() will always look for the name in the actual dataset, not whatever I label within the table
+
+
+###########################    Tutorial 2   ###################################
+
+
+# From R Programming 101 channel https://www.youtube.com/watch?v=AJ0_PCIVXD4
+
+# how to insert distribution graphics within tables
+# A bit on themes
+
+library(gtExtras)
+library(tidyverse)
+
+# Create a summary of your data
+
+iris %>%
+  gt_plt_summary()
+    # he likes that it does all the thinking for us. And that it includes missing values
+
+# Insert icons and graphics into tables
+view(mtcars) # pulls up the dataset into an R tab
+
+mtcars %>%
+  group_by(cyl) %>%   # rows will be the number of cylendars
+  
+  # summarize will create a collumn called Median, Mean, and Distribution
+  summarize(Median = round(median(mpg), 1),
+              # rounded the median of mpg to 1 decimal place
+            Mean = round(mean(mpg),1),
+            Distribution = list(mpg)) %>%
+              # list of mpg (so multiple values)
+ # view()
+  gt() %>% 
+    # above data is piped into gt()
+  gt_plt_dist(Distribution) %>% 
+    # creates a visual distribution of the data within the Distribution variable
+    # because it is a list of numbers, it can create the distribution 
+  gt_theme_guardian() %>%
+    # this is the pre-set look that the table will have
+  tab_header(title = "Miles per gallon statstics")
+    # table header
 
 
 
