e378a76a4096f49d8835d3bf19191a1f84c12c5c
Krista <krista.kenyon@hotmail.com>
2024-08-08 12:44:42 -0500

    Initial commit

---
+++ scripts/draft code/Draft_Script_Fig06_Pandalus-percent.R
@@ -0,0 +1,57 @@
+################################################################################
+################################################################################
+#                   Figure 6 Pandalus Percent - LEARNING CODE
+################################################################################
+################################################################################
+
+
+# Author: Krista Kenyon
+# Date: Aug 7/2024
+
+# Document Purpose: Saving sources and example code I encountered/used while 
+    # creating Figure 6 in case it's useful in the future.
+
+# Document 'Sections' Include:
+    # Creating figure dataset
+        # subsection: tutorials
+    # Table Code
+        # subsection: tutorials
+    # Unused but Interesting code......
+
+
+################################################################################
+#                        Creating figure dataset
+################################################################################
+
+
+### Coding Category
+
+    # (highlight details) website
+        # Tutorial ?
+        # What did I learn or use from it?
+
+
+################################################################################
+#                        Table Formatting - gt package
+################################################################################
+
+
+### Coding Category
+
+    # (highlight details) website
+        # Tutorial ?
+        # What did I learn or use from it?
+
+
+###########################    Tutorial ?   ###################################
+
+
+# From ............... channel website
+
+# brief description
+
+################################################################################
+#                                 Unused Code
+################################################################################
+
+
